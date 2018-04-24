07c2647d6c2ff84af8fc64c9e8712b62ba2915e2
colin <wanglirong972@gmail.com>
2018-04-25 00:16:46 +0800

    threadpool

--- CustomView/app/src/main/java/colin/com/customview/MainActivity.java
+++ CustomView/app/src/main/java/colin/com/customview/MainActivity.java
@@ -3,6 +3,9 @@ package colin.com.customview;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import colin.com.superutil.ExecutorManager;
+import colin.com.superview.CountDownView;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -10,5 +13,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
+        CountDownView countDownView=findViewById(R.id.countDownView);
+        countDownView.start();
+
+
     }
 }
--- CustomView/superutil/src/main/java/colin/com/superutil/ExecutorManager.java
+++ CustomView/superutil/src/main/java/colin/com/superutil/ExecutorManager.java
@@ -1,17 +1,29 @@
 package colin.com.superutil;
 
-public class ThreadPool {
-    private 
-    public static ThreadPool threadPool;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
-    public static ThreadPool getInstace(){
+public class ExecutorManager {
+    private ExecutorService executorService;
+    private static ExecutorManager threadPool;
+
+    public static ExecutorManager getInstace(){
         if(threadPool==null){
-            threadPool=new ThreadPool();
+            threadPool=new ExecutorManager();
         }
         return threadPool;
     }
 
-    public ThreadPool() {
-
+    public ExecutorManager() {
+        executorService = Executors.newCachedThreadPool();
     }
+
+    public ExecutorService getExecutorService() {
+        return executorService;
+    }
+
+    public void cancel(){
+         executorService.shutdown();
+    }
+
 }
